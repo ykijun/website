a590495a91ef140f2e0b509b2aabd36ff492f9ea
Kijun_YU <ykj6865@naver.com>
2020-04-14 19:27:13 +0800

    email part test

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
     $('nav a[href*="#"]').on('click', function () {
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')). offset().top -100
+            scrollTop: $($(this).attr('href')). offset().top - 100
         }, 1500)
     });
 
